fb731d68387f36e5d32ffc733d8152a9693380de
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:41:03 +0530

    Update Sum of both diagonals of matrices.c

--- Sum of both diagonals of matrices.c
+++ Sum of both diagonals of matrices.c
@@ -1,1 +1,44 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("enter number of rows/columns:");
+    scanf("%d", &a);
+
+    int mat[a][a];
+    int sum = 0;
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            printf("enter number in (%d,%d):", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("matrix is-\n");
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            printf("%d\t", mat[i][j]);
+
+            if ((i == j) || ((i + j) == (a - 1)))
+            {
+                sum += mat[i][j];
+            }
+        }
+
+        printf("\n");
+    }
+
+    printf("sum is %d", sum);
+
+    printf("\nNanajinaik,125113032");
+    return 0;
+}
