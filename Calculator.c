771fbe4a8d750703f2946e25307e181751593524
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:06:06 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,1 +1,58 @@
 
+#include <stdio.h>
+
+int main()
+{
+	float a, b;
+	int c;
+
+	printf("ENTER FIRST NUMBER:");
+	scanf("%f", &a);
+
+	printf("ENTER SECOND NUMBER:");
+	scanf("%f", &b);
+
+	printf("ENTER YOUR OPRETION TYPE-\n");
+
+	printf("1 FOR ADDITION\n");
+	printf("2 FOR SUBTRACTION\n");
+	printf("3 FOR MULTIPLICATION\n");
+	printf("4 FOR DIVISION\n");
+
+	scanf("%d", &c);
+
+	switch (c)
+	{
+	case 1:
+		printf("SUM OF BOTH NUMBERS IS %f", a + b);
+		break;
+
+	case 2:
+		printf("SUBSTRACTION OF BOTH NUMBERS IS %f", a + b);
+		break;
+
+	case 3:
+		printf("MULTIPLICATION OF BOTH NUMBERS IS %f", a * b);
+		break;
+
+	case 4:
+		if (b == 0)
+		{
+			printf("erro!! denominator can not 0");
+		}
+		else
+		{
+
+			printf("QUOTIENT OF BOTH NUMBERS IS %f", a / b);
+			
+		}
+		break;
+
+	default:
+		printf("PLEASE ENTER VALID CHOICE");
+	}
+
+	printf("\nNanajinaik,125113032");
+
+	return 0;
+}
