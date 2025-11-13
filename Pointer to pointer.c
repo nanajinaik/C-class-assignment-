2039c9e92819643915bdd963af3ec47dcb6a8182
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:02:55 +0530

    Update Pointer to pointer.c

--- Pointer to pointer.c
+++ Pointer to pointer.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+
+    printf("Enter number: ");
+    scanf("%d", &a);
+
+    int *p, **q;
+    p = &a;
+    q = &p;
+
+    printf("value of number is %d\n", a);
+    printf("address of number %d\n", p);
+    printf("address of pointer %d\n", q);
+
+    printf("\nNanajinaik,125113032"); 
+    return 0;
+}
