da016c490e1e7cfb01f4de258bb7fe8bcb57250f
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 20:50:16 +0530

    Update Writing in file .c

--- Writing in file .c
+++ Writing in file .c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    FILE *f;
+    char text[1000];
+
+    f = fopen("file.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("Enter text that you want to write in file:\n");
+    fgets(text, sizeof(text), stdin);
+
+    fprintf(f, "%s", text);
+    fclose(f);
+    
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
