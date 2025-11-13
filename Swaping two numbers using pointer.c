71aa7ebcaa9fc6de74a44062920c0c39b2de992d
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 20:45:31 +0530

    Update Swaping two numbers using pointer.c

--- Swaping two numbers using pointer.c
+++ Swaping two numbers using pointer.c
@@ -1,1 +1,29 @@
 
+#include<stdio.h>
+
+int main()
+{
+int a, b, *p1 ,*p2;
+
+p1=&a;
+p2=&b;
+
+printf("Enter first number: ");
+scanf("%d",&a);
+
+printf("Enter second number: ");
+scanf("%d",&b);
+
+printf("Before swaping a=%d and b=%d\n",a,b);
+
+int temp;
+temp=*p1;
+*p1=*p2;
+*p2=temp;
+
+printf("After swaping a=%d and b=%d\n",a,b);
+
+printf("\nNanajinaik,125113032");
+
+    return 0;
+}
