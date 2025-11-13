777af6859f73c9f668839e6feebd7e3fcb0faaa9
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 20:48:59 +0530

    Update Transverse array using pointer.c

--- Transverse array using pointer.c
+++ Transverse array using pointer.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{
+    int n, i;
+    int arr[100];
+    int *ptr;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter elements %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    ptr = arr;
+
+    printf("Array elements using pointer arithmetic:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Element %d = %d\n", i + 1, *(ptr + i));
+    }
+
+    printf("\nNanajinaik,125113032"); 
+
+    return 0;
+}
