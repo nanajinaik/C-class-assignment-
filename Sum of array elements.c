790181ed8df660fb017935413c3a0894ecff873a
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:38:35 +0530

    Update Sum of array elements.c

--- Sum of array elements.c
+++ Sum of array elements.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    int arr[100], n, sum = 0;
+    int *ptr;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    ptr = arr;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += *(ptr + i);
+    }
+
+    printf("Sum of array elements = %d\n", sum);
+    printf("Nanajinaik,125113032");
+
+    return 0;
+}
