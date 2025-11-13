64b3427b2bc5e5fe9ecbd813d72dcd474ca3372d
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 20:41:51 +0530

    Update Sum of n natural numbers.c

--- Sum of n natural numbers.c
+++ Sum of n natural numbers.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b;
+
+    printf("enter number to add till it: ");
+    scanf("%d", &b);
+
+    for (int i = 1; i <= b; i++)
+    {
+        a += i;
+    }
+
+    printf("sum of first %d natural numbers is %d", b, a);
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
