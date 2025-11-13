96640f7bf1ef9a2d12dbe1948fcbb5dc3a6ec2ed
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:33:11 +0530

    Update String palindrome check.c

--- String palindrome check.c
+++ String palindrome check.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
 
+void pal(int a, char ch[])
+{
+    char ch2[a + 1];
+
+    for (int i = 0; i < a; i++)
+    {
+        ch2[i] = ch[a - i - 1];
+    }
+
+    ch2[a] = '\0';
+
+    if (strcmp(ch, ch2) == 0)
+    {
+        printf("String is palindrome.\n");
+    }
+
+    else
+    {
+        printf("String is not palindrome.\n");
+    }
+}
+
+int main()
+{
+    char ch[100];
+
+    printf("Enter string: ");
+    scanf("%s", ch);
+
+    int a = strlen(ch);
+    pal(a, ch);
+
+    printf("\nNanajinaik,125113032");
+    return 0;
+}
