80fd50817161034cf57ec2edca47b344fd67d6b2
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 20:40:15 +0530

    Update Sum of digits using recursion.c

--- Sum of digits using recursion.c
+++ Sum of digits using recursion.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+int reverse(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        int digit = num % 10;
+        sum = sum + digit;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    result = reverse(n);
+    printf("Sum of digiits is: %d", result);
+
+    printf("\nNanajinaik,125113032");
+    return 0;
+}
