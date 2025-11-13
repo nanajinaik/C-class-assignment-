928460c97f05e522002e4d2dd134af0ef7999b55
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:35:29 +0530

    Update Student with highest marks.c

--- Student with highest marks.c
+++ Student with highest marks.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
+#include <string.h>
 
+struct student
+{
+    char name[50];
+    int marks;
+};
+
+int main()
+{
+    int a;
+
+    printf("NUMBER OF STUDENTS:");
+    scanf("%d", &a);
+
+    struct student ch[a];
+
+    for (int i = 0; i < a; i++)
+    {
+
+        printf("FOR STUDENT %d\n", i + 1);
+
+        printf("ENTER NAME OF STUDENT: ");
+        scanf("%s", ch[i].name);
+
+        printf("ENTER MARKS OF STUDENT :");
+        scanf("%d", &ch[i].marks);
+    }
+
+    for (int i = 0; i < a - 1; i++)
+    {
+
+        if (ch[i].marks > ch[i + 1].marks)
+        {
+            int temp = ch[i].marks;
+            ch[i].marks = ch[i + 1].marks;
+            ch[i + 1].marks = temp;
+
+            char t[50];
+            strcpy(t, ch[i].name);
+            strcpy(ch[i].name, ch[i + 1].name);
+            strcpy(ch[i + 1].name, t);
+        }
+    }
+
+    printf("STUDENT WITH MAXIMUM MARKS -\n");
+    printf("NAME: %s\n", ch[a - 1].name);
+    printf("MARKS: %d\n", ch[a - 1].marks);
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
