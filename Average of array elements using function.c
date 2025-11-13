e6bd4f84fb092a6ab7ef284388d0950549e699c7
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 17:56:58 +0530

    Update Average of array elements using function.c

--- Average of array elements using function.c
+++ Average of array elements using function.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int avg(int a, int arr[])
+{
+    int sum = 0;
+    for (int j = 0; j < a; j++)
+    {
+        sum += arr[j];
+    }
+    return sum;
+}
+
+int main()
+{
+    int a;
+    printf("Enter number of elements: ");
+    scanf("%d", &a);
+
+    int arr[a];
+
+    for (int i = 0; i < a; i++)
+    {
+        printf("Enter element: ");
+        scanf("%d", &arr[i]);
+    }
+    float b = avg(a, arr);
+    printf("Average of all numbers is %f", b / a);
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
