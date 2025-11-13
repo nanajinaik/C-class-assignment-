f44ddc623b55bf46a4967cb38b7eb3e93982f999
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:07:23 +0530

    Update Prime number using function.c

--- Prime number using function.c
+++ Prime number using function.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+int prime(int a)
+{
+    int b = 1;
+
+    if (a == 1 || a == 0)
+        b = 0;
+
+    else
+    {
+        for (int i = 2; i < a; i++)
+        {
+            if (a % i == 0)
+            {
+                b = 0;
+                break;
+            }
+        }
+    }
+    return b;
+}
+
+int main()
+{
+    int a;
+
+    printf("Enter your number: ");
+    scanf("%d", &a);
+
+    if (prime(a))
+    {
+        printf("number is prime");
+    }
+
+    else
+    {
+        printf("number is not prime");
+    }
+
+    printf("\nNanajinaik,125113032"); 
+
+    return 0;
+}
