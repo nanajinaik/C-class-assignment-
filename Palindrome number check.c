1501301f9b74138b446c6b352cd838e7f9888145
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 19:01:29 +0530

    Update Palindrome number check.c

--- Palindrome number check.c
+++ Palindrome number check.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main()
+{
+    int num, result,n;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    n=num;
+
+    int rev = 0;
+    while (num != 0)
+    {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num = num / 10;
+    }
+
+    if (n == rev)
+    {
+        printf("Number is palindrome.");
+    }
+
+    else
+    {
+        printf("Number is not palindrome.");
+    }
+
+    printf("\nNanajinaik,125113032");
+    return 0;
+}
