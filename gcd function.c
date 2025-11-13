84b148538e8365935ef45543aa2f0af2f9dbbda4
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:25:17 +0530

    Update gcd function.c

--- gcd function.c
+++ gcd function.c
@@ -1,1 +1,35 @@
 
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+
+        int r;
+
+        r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+int main()
+{
+    int a, b;
+
+    printf("enter first number:");
+    scanf("%d", &a);
+
+    printf("enter second number:");
+    scanf("%d", &b);
+
+    int c = gcd(a, b);
+
+    printf("gcd is %d", c);
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
