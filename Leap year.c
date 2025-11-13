10d0456d88473b179c1186c4f0702a2eacd0178e
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:45:06 +0530

    Update Leap year.c

--- Leap year.c
+++ Leap year.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    printf("ENTER YEAR :");
+    scanf("%d", &a);
+
+    if (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0))
+    {
+        printf("YEAR IS LEAP YEAR");
+    }
+
+    else
+    {
+        printf("YEAR IS NOT A LEAP YEAR");
+    }
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
