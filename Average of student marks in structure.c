2c3e2c60fbdc4b2472810ac74e69eeb026a5dee5
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 17:58:52 +0530

    Update Average of student marks in structure.c

--- Average of student marks in structure.c
+++ Average of student marks in structure.c
@@ -1,1 +1,39 @@
 
+#include <stdio.h>
+
+struct student
+{
+    int marks;
+};
+
+int main()
+{
+    int a, sum = 0;
+    float avg;
+
+    printf("NUMBER OF STUDENTS:");
+    scanf("%d", &a);
+
+    struct student ch[a];
+
+    for (int i = 0; i < a; i++)
+    {
+
+        printf("FOR STUDENT %d\n", i + 1);
+        printf("ENTER MARKS OF STUDENT :");
+        scanf("%d", &ch[i].marks);
+    }
+
+    for (int j = 0; j < a; j++)
+    {
+        sum += ch[j].marks;
+    }
+
+    avg = (float)sum / a;
+
+    printf("AVERAGE MARKS OF ALL STUDENTS ARE %f", avg);
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
