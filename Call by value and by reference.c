00d0630296b0e5b88bc02fdb56c6266d824d9286
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:07:42 +0530

    Update Call by value and by reference.c

--- Call by value and by reference.c
+++ Call by value and by reference.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    int a;
+    a = 10;
+
+    int *ptr = &a;
+    printf("number is %d\n", a);    // call by valaue
+    printf("number is %d\n", *ptr); // call by reference
+    
+    printf("\nNanajinaik,125113032");
+    return 0;
+}
