a45f257628ffb77dfdfde68e31f29b9a1031bd8e
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:46:30 +0530

    Update Length of array using pointer.c

--- Length of array using pointer.c
+++ Length of array using pointer.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char str[100];
+    int l = 0;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        l++;
+    }
+
+    printf("string lenth is %d", l);
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
