ae61ba0e1e843cb4c811d5b3b0f1a66a1d55026c
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 17:53:36 +0530

    Update Append data to a file.c

--- Append data to a file.c
+++ Append data to a file.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int main()
+{
+    char text[100];
+    FILE *fp;
+
+    fp = fopen("file2.txt", "a");
+    if (fp == NULL)
+    {
+        printf("!! ERROR OPENING FILE FOR WRITING\n");
+        return 1;
+    }
+
+    printf("ENTER TEXT TO APPEND TO FILE: ");
+    fgets(text, sizeof(text), stdin);
+    fprintf(fp, "%s", text);
+
+    fclose(fp);
+
+    fp = fopen("file.txt", "r");
+    if (fp == NULL)
+    {
+        printf("!! ERROR OPENING FILE FOR READING\n");
+        return 1;
+    }
+
+    printf("\nCONTENT OF FILE:\n");
+
+    while (fgets(text, sizeof(text), fp) != NULL)
+    {
+        printf("%s", text);
+    }
+
+    fclose(fp);
+
+    printf("\nNanajinaik,125113032");
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
