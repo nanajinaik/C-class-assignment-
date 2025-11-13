ec8e66bc7ecb76979739b3837db8d6e492e5da70
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 08:42:51 +0530

    Create Even odd.c

---
+++ Even odd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("ENTER NUMBER TO CHECK EVEN OR ODD: ");
+	scanf("%d", &a);
+	
+	if(a%2==0)
+	{
+		printf("NUMBER IS EVEN");
+	}
+	
+    else
+    {
+  	printf("NUMBER IS ODD");
+	
+	}
+	
+	printf("\nNanajinaik,125113032");
+	
+	return 0;
+}
