d44f8980b6df8bdbc4ade7ab527addc3692f89e7
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:21:36 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main()
+{
+
+	int a;
+
+	printf("ENTER NUMBER TO FINT FACTORIAL:");
+	scanf("%d", &a);
+	if (a == 0)
+	{
+		a = 1;
+	}
+
+	else if (a < 0)
+	{
+		printf("FACTORIAL IS NOT DEFINE FOR NEGATIVE NUMBER");
+	}
+
+	else
+	{
+
+		int i;
+		for (i = a; i > 1; i--)
+		{
+			a = a * (i - 1);
+		}
+
+		printf("FACTORIAL IS %d", a);
+	}
+
+	printf("\nNanajinaik,125113032");
+	return 0;
+}
