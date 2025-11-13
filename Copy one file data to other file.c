10e3ca925cac430f4e283b695d455e269ee2f12a
nanajinaik <nani42obanavathu@gmail.com>
2025-11-13 18:11:37 +0530

    Update Copy one file data to other file.c

--- Copy one file data to other file.c
+++ Copy one file data to other file.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    FILE *f;
+    char text[1000];
+    
+    f = fopen("file1.txt", "r");
+    if (f == NULL) {
+        printf("Error opening file for reading!\n");
+        return 1;
+    }
+
+    
+    while (fgets(text, sizeof(text), f) != NULL);
+    
+    fclose(f);
+    FILE *p;
+
+    p=fopen("file2.txt","w");
+
+    fprintf(p,"%s",text);
+    fclose(p);
+
+    printf("\nText written to file successfully.\n");
+
+    printf("\nNanajinaik,125113032");
+
+    return 0;
+}
